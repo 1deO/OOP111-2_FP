df7cae64f1fce63638c41f18eb0a7e90541e0334
gokun123 <120576403+gokun123@users.noreply.github.com>
2023-06-09 15:55:04 +0800

    Update WelcomePage.java

--- WelcomePage.java
+++ WelcomePage.java
@@ -6,6 +6,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
@@ -17,27 +19,35 @@ import java.awt.SystemColor;
 public class WelcomePage extends JFrame {
 
 	private JPanel contentPane;
+	Connection conn;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					WelcomePage frame = new WelcomePage();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		String server = "jdbc:mysql://140.119.19.73:3315/";
+		String database = "111306079"; // change to your own database
+		String url = server + database + "?useSSL=false";
+		String username = "111306079"; // change to your own user name
+		String password = "d7w00"; // change to your own password
+
+		try {
+			Connection conn = DriverManager.getConnection(url, username, password);
+			System.out.println("DB Connectd");
+
+			WelcomePage frame = new WelcomePage(conn);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public WelcomePage() {
+	public WelcomePage(Connection conn) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 400);
 		contentPane = new JPanel();
@@ -45,7 +55,7 @@ public class WelcomePage extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		
+
 		JButton btnBuyer = new JButton("買家Buyer");
 		btnBuyer.setBackground(new Color(224, 255, 255));
 		btnBuyer.setBounds(65, 250, 150, 50);
@@ -65,7 +75,7 @@ public class WelcomePage extends JFrame {
 		});
 		contentPane.setLayout(null);
 		contentPane.add(btnBuyer);
-		
+
 		JButton btnSeller = new JButton("賣家Seller");
 		btnSeller.setBackground(new Color(224, 255, 255));
 		btnSeller.setBounds(285, 250, 150, 50);
@@ -74,7 +84,7 @@ public class WelcomePage extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				GUI_Seller sellerFrame;
 				try {
-					sellerFrame = new GUI_Seller();
+					sellerFrame = new GUI_Seller(conn);
 					sellerFrame.setVisible(true);
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
@@ -83,7 +93,7 @@ public class WelcomePage extends JFrame {
 			}
 		});
 		contentPane.add(btnSeller);
-		
+
 		JLabel lbWelcome = new JLabel("Welcome");
 		lbWelcome.setBounds(184, 116, 136, 53);
 		lbWelcome.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 20));
