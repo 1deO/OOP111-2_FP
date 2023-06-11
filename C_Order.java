fbd92152bc681d9822e21f9feb7b083643f9f89c
NOOTNOOTPINGUUU <111306079@g.nccu.edu.tw>
2023-06-11 18:59:26 +0800

    Update C_Order.java
    addtocart功能 和介面優化

--- C_Order.java
+++ C_Order.java
@@ -5,9 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,6 +25,7 @@ import javax.swing.SpinnerNumberModel;
 public class C_Order extends JFrame {
 
 	private JPanel contentPane;
+	private String storeID;
 	Statement stat;
 
 	/**
@@ -88,6 +92,44 @@ public class C_Order extends JFrame {
 		taCart.setBounds(460, 55, 205, 330);
 		contentPane.add(taCart);
 		
+		JComboBox firmCombo = new JComboBox();
+		firmCombo.setBackground(new Color(250, 250, 250));
+		firmCombo.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 20));
+		firmCombo.setModel(new DefaultComboBoxModel(new String[] {"集英樓", "政大小吃部", "盛奇士", "H.I.Feeling", "伊果咖啡", "越南麵包"
+				, "貍66日式炒麵", "新巨輪海海人生", "日式行丼餐車", "胖都日式脆皮雞蛋糕"}));
+		firmCombo.setBounds(200, 10, 130, 40);
+		contentPane.add(firmCombo);
+		
+		JButton confirmButton = new JButton("選取");
+		confirmButton.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 20));
+		confirmButton.setBounds(350, 10, 80, 40);
+		confirmButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				String firm = (String)firmCombo.getSelectedItem();
+				switch(firm){
+					case "集英樓" : storeID = "A1"; break;
+					case "政大小吃部" : storeID = "A2"; break;
+					case "盛奇士" : storeID = "A3"; break;
+					case "H.I.Feeling" : storeID = "A4"; break;
+					case "伊果咖啡" : storeID = "A5"; break;
+					case "越南麵包" : storeID = "B6"; break;
+					case "貍66日式炒麵" : storeID = "B7"; break;
+					case "新巨輪海海人生" : storeID = "B8"; break;
+					case "日式行丼餐車" : storeID = "B9"; break;
+					case "胖都日式脆皮雞蛋糕" : storeID = "B10"; break;
+				}
+				String query1 = "SELECT ID,Product,Price FROM `menu` WHERE StoreID = '" + storeID + "'";
+				try {
+					ResultSet result1 = stat.executeQuery(query1);
+					taMenu.setText(showResultSet_3(result1));
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		contentPane.add(confirmButton);
+		
 		JButton btnReturn = new JButton("返回 Return");
 		btnReturn.setBounds(30, 400, 150, 40);
 		btnReturn.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 20));
@@ -104,16 +146,6 @@ public class C_Order extends JFrame {
 			}
 		});
 		contentPane.add(btnReturn);
-		
-		JButton btnAddToCart = new JButton("加入購物車 AddToCart");
-		btnAddToCart.setBounds(190, 400, 245, 40);
-		btnAddToCart.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 20));
-		btnAddToCart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//add to database
-			}
-		});
-		contentPane.add(btnAddToCart);
 		
 		JButton btnPayment = new JButton("結帳 Payment");
 		btnPayment.setBounds(465, 400, 195, 40);
@@ -155,5 +187,94 @@ public class C_Order extends JFrame {
 		spinner_ID.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 20));
 		contentPane.add(spinner_ID);
 		
+		JButton btnAddToCart = new JButton("加入購物車 AddToCart");
+		btnAddToCart.setBounds(190, 400, 245, 40);
+		btnAddToCart.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 20));
+		btnAddToCart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					try {
+						spinner_ID.commitEdit();
+					} catch ( java.text.ParseException e2 ) { 
+						
+					}
+					int ID = (Integer)spinner_ID.getValue();
+					String Product=new String();
+					int amount = (Integer)spinner_q.getValue();
+					int singlePrice = 0;
+					
+					String query = "SELECT Product FROM `menu` WHERE ID = '" + ID + "'";
+					ResultSet result = stat.executeQuery(query);
+					while(result.next()){
+							Product = String.format("%s", result.getString(1));
+					}
+					query = "SELECT Price FROM `menu` WHERE ID = '" + ID + "'";
+					result = stat.executeQuery(query);
+					while(result.next()){
+							singlePrice = result.getInt(1);
+					}
+					taCart.append(String.format("ID: %s %s \n$%s amount:%s",ID,Product,singlePrice,amount));
+					String sql = "INSERT INTO `ShoppingCart` ( `Product`, `amount`, `singlePrice`) VALUES ('"
+							+ Product + "', '" + amount + "','" + singlePrice + "');";
+					stat.executeUpdate(sql);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		contentPane.add(btnAddToCart);
+		
+	}
+	public static String showResultSet_1(ResultSet result) throws SQLException {
+		
+		ResultSetMetaData metaData = result.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		
+		String output = "";
+		
+		for (int i = 1; i <= columnCount; i++) {
+			output += String.format("%15s", metaData.getColumnLabel(i));
+		}
+		output += "\n";
+		while (result.next()) {
+			for (int i = 1; i <= columnCount; i++) {
+				output += String.format("%15s", result.getString(i));
+			}
+			output += "\n";
+		}
+		return output;
+	}
+	public static String showResultSet_2(ResultSet result) throws SQLException {
+		ResultSetMetaData metaData = result.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		String output = "";
+		while(result.next()){
+			for (int i = 1; i <= columnCount; i++) {
+				output += String.format("%10s", metaData.getColumnLabel(i));
+				output += String.format(" %s\n", result.getString(i));
+			}
+		}
+		output += "\n";
+		return output;
+	}
+	public static String showResultSet_3(ResultSet result) throws SQLException {
+		ResultSetMetaData metaData = result.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		String output = "";
+		while(result.next()){
+			for (int i = 1; i <= columnCount; i++) {
+				if(i%3==1) {
+					output += String.format("ID: %s", result.getString(i));
+				}
+				else if(i%3==2) {
+					output += String.format(" %-5s", result.getString(i));
+				}
+				else {
+					output += String.format(" $%s\n", result.getString(i));
+				}
+			}
+		}
+		return output;
 	}
 }
